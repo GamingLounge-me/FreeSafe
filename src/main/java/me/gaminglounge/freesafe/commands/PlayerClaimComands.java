70fb76803804948bd2f017f9f39abd8a3f836483
Jan Tom Förster <jan_tom@f-rster.de>
2025-01-14 11:22:58 +0100

    added the radius command and testing some messages, but does still not work due to WorldEdit bug.

--- src/main/java/me/gaminglounge/freesafe/commands/PlayerClaimComands.java
+++ src/main/java/me/gaminglounge/freesafe/commands/PlayerClaimComands.java
@@ -9,10 +9,12 @@ import dev.jorel.commandapi.arguments.StringArgument;
 import dev.jorel.commandapi.wrappers.Location2D;
 import me.gaminglounge.freesafe.FreeSafe;
 import me.gaminglounge.freesafe.VariableManager;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 
 public class PlayerClaimComands {
 
     FreeSafe freeSafe = FreeSafe.INSTANCE;
+    MiniMessage mm = MiniMessage.miniMessage();
 
     public PlayerClaimComands() {
         new CommandAPICommand("claim")
@@ -22,15 +24,22 @@ public class PlayerClaimComands {
                 .withArguments(new Location2DArgument("pos1"))
                 .withArguments(new Location2DArgument("pos2"))
                 .executesPlayer((player, args)->{
+                    if (args.get("ClaimName") == null) {
+                        player.sendMessage(mm.deserialize("<white>[</white><gradient:aqua,green>GamingLounge</gradient><white>]</white> <red>You must provide a name for the claim</red>"));
+                        return;
+                    }
                     Location pos3 = VariableManager.pos1To3D((Location2D)args.get("pos1"));
                     Location pos4 = VariableManager.pos2To3D((Location2D)args.get("pos2"));
                     FreeSafe.INSTANCE.createClaim.createRegion(player.getPlayer(), (String)args.get("ClaimName"), pos3, pos4);
                 })    
-/*                .withSubcommand(new CommandAPICommand("radius"))
+                .withSubcommand(new CommandAPICommand("radius"))
+                .withArguments(new StringArgument("ClaimName"))
                 .withArguments(new IntegerArgument("radius"))
                 .executesPlayer((player, args)->{
-                    // FreeSafe.INSTANCE.createClaim.createRegion(player.getPlayer(), (String)args.get("ClaimName"));
-                })*/
+                    Location pos3 = VariableManager.pos3FromRadius((Location)player.getLocation(), (Integer)args.get("radius"));
+                    Location pos4 = VariableManager.pos4FromRadius((Location)player.getLocation(), (Integer)args.get("radius"));
+                    FreeSafe.INSTANCE.createClaim.createRegion(player.getPlayer(), (String)args.get("ClaimName"), pos3, pos4);
+                })
         .register();
     }
 }
